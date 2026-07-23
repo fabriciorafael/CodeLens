bd337b3e96e0cdde98d3e48fc04eaa0aeb697849
fabricio rafael <142851247+fabriciorafael@users.noreply.github.com>
2026-07-22 23:21:59 -0300

    Cria estrutura inicial da interface

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
-    <>
-    
-    </>
+    <main>
+       <div className="container">
+        <h1 className="title">
+          Analisador de código com IA
+        </h1>
+        <p></p>
+
+
+       </div>
+    </main>
   )
 }
 
